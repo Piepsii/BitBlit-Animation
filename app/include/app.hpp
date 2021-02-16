5b0061dc1f31021fd869b2471549d75ccb79a42f
Piepsii <paulbrandstetter00@gmail.com>
2021-02-16 21:54:30 +0100

    implemented basic functions

--- app/include/app.hpp
+++ app/include/app.hpp
@@ -5,64 +5,116 @@
 
 #include <lime.hpp>
 
-namespace lime {
-   namespace bitmap {
-      bool valid(const Bitmap &image)
-      {
-         return image.data != nullptr;
-      }
+namespace lime
+{
+	namespace bitmap
+	{
+		bool valid(const Bitmap &image)
+		{
+			return image.data != nullptr;
+		}
 
-      bool create(const int width, const int height, Bitmap &image)
-      {
-         // todo: check if image has previously been allocated, 
-         //       if so destroy it and do the below
-         // todo: allocate the bitmap pixel data using 'width' and 'height'.
+		bool create(const int width, const int height, Bitmap &image)
+		{
+			// todo: check if image has previously been allocated, 
+			//       if so destroy it and do the below
+			if( valid(image) )
+			{
+				destroy(image);
+			}
+			// todo: allocate the bitmap pixel data using 'width' and 'height'.
+			int size = width * height;
+			image.width = width;
+			image.height = height;
+			image.data = new Color[size];
 
-         return valid(image);
-      }
+			return valid(image);
+		}
 
-      bool inside(const Bitmap &image, const int x, const int y)
-      {
-         // todo: is the image valid?
-         // todo: checks if 'x' and 'y' is inside the bitmap.
+		bool inside(const Bitmap &image, const int x, const int y)
+		{
+			// todo: is the image valid?
+			if( !valid(image) )
+			{
+				return false;
+			}
+			// todo: checks if 'x' and 'y' is inside the bitmap.
+			int size = image.width * image.height;
+			return y * image.width + x <= size && !(x < 0) && !(y < 0) ? true : false;
+		}
 
-         return false;
-      }
+		Color get(const Bitmap &image, const int x, const int y)
+		{
+			const Color magenta{ 255, 0, 255, 255 };
+			// todo: is the image valid?
+			if( !valid(image) )
+			{
+				return magenta;
+			}
+			// todo: is pixel inside the image?
+			if( !inside(image, x, y) )
+			{
+				return magenta;
+			}
+			// todo: calculate the correct index for 'x' and 'y' in the 
+			//       bitmap and return requested pixel color. 
+			int index = y * image.width + x;
+			return image.data[index];
+		}
 
-      Color get(const Bitmap &image, const int x, const int y)
-      {
-         const Color magenta{ 255, 0, 255, 255 };
-         // todo: is the image valid?
-         // todo: is pixel inside the image?
-         // todo: calculate the correct index for 'x' and 'y' in the 
-         //       bitmap and return requested pixel color. 
+		void set(Bitmap &image, const int x, const int y, const Color &color)
+		{
+			// todo: is the image valid?
+			if( !valid(image) )
+			{
+				return;
+			}
+			// todo: is pixel inside the image?
+			if( !inside(image, x, y) )
+			{
+				return;
+			}
+			// todo: calculate the correct index for 'x' and 'y' in bitmap
+			//       and set it to color 'c'
+			int index = y * image.width + x;
+			image.data[index] = color;
+		}
 
-         return magenta;
-      }
+		void clear(Bitmap &image, const Color &color)
+		{
+			if( !valid(image) )
+			{
+				return;
+			}
+			// todo: set all bitmap pixels to color 'c'
+			for( int i = 0; i < image.width * image.height; i++ )
+			{
+				image.data[i] = color;
+			}
+		}
 
-      void set(Bitmap &image, const int x, const int y, const Color &color)
-      {
-         // todo: is the image valid?
-         // todo: is pixel inside the image?
-         // todo: calculate the correct index for 'x' and 'y' in bitmap
-         //       and set it to color 'c'
-      }
+		void blit(const Bitmap &src, const Rectangle &rect, Bitmap &dst, const Point &pos)
+		{
+			// todo: blit a portion of 'src' specified by 'rect' onto the destination
+			//       'dst' at the specified point 'pos'.
+			if( !valid(src) ||
+			   !valid(dst) ||
+			   rect.height < 0 ||
+			   rect.width < 0 ||
+			   pos.x > dst.width ||
+			   pos.x < 0 ||
+			   pos.y > dst.height ||
+			   pos.y < 0 )
+			{
+				return;
+			}
+			// todo: find rectangle in src
 
-      void clear(Bitmap &image, const Color &color)
-      {
-         if (!valid(image)) {
-            return;
-         }
+			// todo: find pos in dst
 
-         // todo: set all bitmap pixels to color 'c'
-      }
-
-      void blit(const Bitmap &src, const Rectangle &rect, Bitmap &dst, const Point &pos)
-      {
-         // todo: blit a portion of 'src' specified by 'rect' onto the destination
-         //       'dst' at the specified point 'pos'.
-      }
-   } // !image
+			// todo: map rectangle onto dst
+		}
+	} // !image
 } // !lime
 
 #endif // !APP_HPP_INCLUDED
--- app/source/app.cc
+++ app/source/app.cc
@@ -4,30 +4,44 @@
 
 int main(int argc, char **argv)
 {
-   using namespace lime; // note: window has to be 1280x720
-   if (window::open(1280, 720, "lime")) {
-      Bitmap tileset{};
-      if (!bitmap::create("assets/explosion.bmp", tileset)) {
-         return false;
-      }
+	using namespace lime; // note: window has to be 1280x720
+	if( window::open(1280, 720, "lime") )
+	{
+		Bitmap tileset{};
+		if( !bitmap::create("assets/explosion.bmp", tileset) )
+		{
+			return false;
+		}
 
-      Bitmap image{}; // note: has to be 320x180
-      if (!bitmap::create(320, 180, image)) {
-         return false;
-      }
+		Bitmap image{}; // note: has to be 320x180
+		if( !bitmap::create(320, 180, image) )
+		{
+			return false;
+		}
 
-      // todo: construct frames for animation
+		// todo: construct frames for animation
+		Bitmap frames[12];
+		for( int i = 0; i < 12; i++ )
+		{
+			if( !bitmap::create(96, 96, frames[i]) )
+			{
+				return false;
+			}
 
-      while (window::process()) {
-         if (keyboard::pressed(keyboard::Key::Escape)) {
-            break;
-         }
+		}
 
-         // todo: blit frame animation looping
+		while( window::process() )
+		{
+			if( keyboard::pressed(keyboard::Key::Escape) )
+			{
+				break;
+			}
 
-         window::display(image);
-      }
-   }
+			// todo: blit frame animation looping
 
-   return 0;
+			window::display(frames[0]);
+		}
+	}
+
+	return 0;
 }
